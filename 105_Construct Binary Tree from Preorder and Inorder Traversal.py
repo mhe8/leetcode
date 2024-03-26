812e0e9adaf95b77e4e608d69752f325f56282d8
Min Chen <pkuaaron@gmail.com>
2024-03-26 00:20:11 -0400

    day 2, sql + binary tree

---
+++ 105_Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,37 @@
+"""
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the 
+inorder traversal of the same tree, construct and return the binary tree.
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+Time complexity: O(n)
+Space complexity: O(n)
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        #create a new tree
+        root = TreeNode(preorder[0])
+        # Get the position of the root in the inorder
+        mid  = inorder.index(preorder[0])
+
+        # Traverse
+        root.left = self.buildTree(preorder[1: mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1 : ], inorder[ mid + 1 :])
+
+        return root
---
+++ 180_Consecutive Numbers.sql
@@ -0,0 +1,52 @@
+"""
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+"""
+
+
+with cte as
+(
+    SELECT 
+    lag(num) over (order by id) as before_num, num,
+    lead(num) over (order by id) as after_num
+    FROM logs
+)
+
+SELECT distinct  num as  ConsecutiveNums
+from cte
+where num=before_num and num=after_num;
